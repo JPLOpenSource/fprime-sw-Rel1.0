be5abf30450330bbc27f2c52d2046594b8f87111
ssinha <sanchit.sinha@jpl.nasa.gov>
2017-06-28 08:13:35 -0700

    Created combined Dictionary in python_to_json

--- Gse/src/pyth_to_json.py
+++ Gse/src/pyth_to_json.py
@@ -12,5 +12,23 @@ print dir_path
 cl = channel_loader.ChannelLoader.getInstance()
 cl.create(dir_path)
 
+combined_dict = {}
+for id in cl.getNameDict():
+	combined_dict[id] = {}
+	combined_dict[id]["name"] = cl.getNameDict()[id]
+
+for id in combined_dict:
+	combined_dict[id]["component"] = cl.getCompDict()[id]
+
+for id in combined_dict:
+	combined_dict[id]["description"] = cl.getChDescDict()[id]
+
+for id in combined_dict:
+	combined_dict[id]["type"] = cl.getTypesDict()[id]
+
+for id in combined_dict:
+	combined_dict[id]["format"] = cl.getFormatStringDict()[id]
+
+
 if __name__ == "__main__":
-	print cl.getChDescDict()
+	print combined_dict
