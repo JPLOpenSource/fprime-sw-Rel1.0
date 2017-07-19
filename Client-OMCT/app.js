0ef19466bdca3f1c133783b9ca03baf4c2bdbd6b
ssinha <sanchit.sinha@jpl.nasa.gov>
2017-07-19 16:02:10 -0700

    Created fixed chart script

--- Client-OMCT/app.js
+++ Client-OMCT/app.js
@@ -5,6 +5,7 @@
 var StaticServer = require('./server/static-server');
 var RealtimeIsfServer = require('./server/telemetry-isf');
 var HistoryIsfServer = require('./server/history-server');
+var CreateFixed = require('./server/create-fixed');
 
 const OMCTPort = 8080;
 // Create static server for client
@@ -17,3 +18,5 @@ const gsePort = 50000;
 const site = '127.0.0.1';
 RealtimeIsfServer(site, gsePort, realMctPort);
 HistoryIsfServer(site, histMctPort);
+
+CreateFixed();
---
+++ Client-OMCT/server/create-fixed.js
@@ -0,0 +1,72 @@
+// Create fixed display of all channels
+
+var getIds = require('./deserializeIsf').getIds;
+var fs = require('fs');
+
+function CreateFixed() {
+
+	var type = 'isf.taxonomy:';
+
+	var idSize = getIds().length;
+	var composition = getIds().map(function (id) {
+		return type + id.toString()
+	});
+
+	var y = -1;
+	var elements = [];
+	composition.forEach(function (id) {
+		y++;
+		elements.push({
+			"type": "fixed.telemetry",
+			"x": 0,
+			"y": y,
+			"id": id,
+			"stroke": "transparent",
+			"color": "",
+			"titled": true,
+			"width": 6,
+			"height": 1,
+			"useGrid": true
+		});
+	});
+
+	var fixed = {
+		"mine": {
+		  "name": "My Items",
+		  "type": "folder",
+		  "composition": [
+		    "267f72c9-22c3-4d7c-ad5e-e471e76e71be"
+		  ],
+		  "location": "ROOT",
+		  "modified": 1500501635491,
+		  "persisted": 1500501635491
+		},
+
+		"267f72c9-22c3-4d7c-ad5e-e471e76e71be": {
+			"layoutGrid": [
+			  64,
+			  idSize + 5
+			],
+			"composition": composition,
+			"name": "Fixed Display Channels",
+			"type": "telemetry.fixed",
+			"configuration": {
+				"fixed-display": {
+					"elements": elements
+				}
+			},
+			"modified": Date.now(),
+			"location": "mine",
+			"persisted": Date.now()
+		}
+	};
+
+	fs.writeFile('server/res/fixed.json', JSON.stringify(fixed), function (err) {
+		if (err) {
+			console.log(err);
+
+		}
+	});
+}
+
+module.exports = CreateFixed;
