8da306370063f43a9f82f06349b357a55c10ccb0
David <david.kooi@jpl.nasa.gov>
2017-07-25 11:49:27 -0700

    ZmqSocketIf component successfully receives commands

--- Ref/ZmqSockIf/ZmqSocketIfImpl.cpp
+++ Ref/ZmqSockIf/ZmqSocketIfImpl.cpp
@@ -1,3 +1,4 @@
+#include <Fw/Com/ComPacket.hpp>
 #include <Fw/Types/EightyCharString.hpp>
 #include <Ref/ZmqSockIf/ZmqSocketIfImpl.hpp>
 
@@ -10,7 +11,32 @@ namespace Ref {
     /////////////////////////////////////////////////////////////////////
     namespace {
 
-        NATIVE_INT_TYPE socketWrite(NATIVE_INT_TYPE fd, U8* buf, U32 size) {
+    	bool zmqError(const char* from) {
+			switch (zmq_errno()) {
+				case EAGAIN:
+				    printf("%s: ZMQ EAGAIN", from);
+				    return true;
+				case EFSM:
+				     printf("%s: ZMQ EFSM", from);
+				     return true;
+				case ETERM:
+					printf("%s: ZMQ terminate\n",from);
+					return true;
+				case ENOTSOCK:
+					printf("%s: ZMQ ENOTSOCK\n",from);
+					return true;
+				case EINTR:
+					printf("%s: ZMQ EINTR\n",from);
+					return false;
+				case EFAULT:
+					printf("%s: ZMQ EFAULT\n",from);
+				default:
+					printf("%s: ZMQ error: %s\n",from,zmq_strerror(zmq_errno()));
+					return true;
+			}
+		}
+
+        NATIVE_INT_TYPE socketWrite(void* zmqSocket, U8* buf, NATIVE_INT_TYPE size) {
             NATIVE_INT_TYPE total=0;
             while(size > 0) {
 
@@ -18,13 +44,30 @@ namespace Ref {
             return total;
         }
 
-        NATIVE_INT_TYPE socketRead(NATIVE_INT_TYPE fd, U8* buf, U32 size) {
+        NATIVE_INT_TYPE zmqSocketRead(void* zmqSocket, U8* buf, NATIVE_INT_TYPE size) {
             NATIVE_INT_TYPE total=0;
-            while(size > 0) {
 
-	    }
+            // Ignore the zmq identifier
+			zmq_msg_t zmqID;
+	    	zmq_msg_init(&zmqID);
+	    	zmq_msg_recv(&zmqID, zmqSocket, 0);
+	    	zmq_msg_close(&zmqID);
+
+	    	// Receive FPrime packet
+	    	zmq_msg_t fPrimePacket;
+	    	zmq_msg_init(&fPrimePacket);
+	    	total = zmq_msg_recv(&fPrimePacket, zmqSocket, 0);
+
+	    	if(total == -1){
+				zmqError("zmqSocketRead");
+	    	}
+
+	    	memcpy(buf, zmq_msg_data(&fPrimePacket), total);
+	    	zmq_msg_close(&fPrimePacket);
+
             return total;
         }
+    
     }
 
     /////////////////////////////////////////////////////////////////////
@@ -70,9 +113,7 @@ namespace Ref {
                	registerToServer(port_number);
 
                 // Spawn read task:
-        	Os::Task::TaskStatus stat = this->socketTask.start(name,0,
-				            priority,10*1024,
-				            ZmqSocketIfImpl::socketReadTask, (void*) this);
+        	Os::Task::TaskStatus stat = this->socketTask.start(name,0, priority,10*1024, ZmqSocketIfImpl::socketReadTask, (void*) this);
         	FW_ASSERT(Os::Task::TASK_OK == stat,static_cast<NATIVE_INT_TYPE>(stat));
         }
     }
@@ -157,13 +198,12 @@ namespace Ref {
 		
 
 		// Create publish socket
-		char endpoint[256];
 		(void)snprintf(endpoint,256,"tcp://%s:%d",this->m_hostname, this->m_serverSubscribePort);
 		// null terminate
         endpoint[255] = 0;
 		this->m_pubSocket = zmq_socket(this->m_zmqContext, ZMQ_DEALER);
 		zmq_setsockopt(this->m_pubSocket, ZMQ_IDENTITY, (const char*)id, strlen((const char*)id));
-		NATIVE_INT_TYPE stat = zmq_connect(this->m_cmdSocket,endpoint);
+		stat = zmq_connect(this->m_pubSocket,endpoint);
 		if (-1 == stat) {
 		  printf("ZMQBind Error\n");
 		  return;
@@ -171,8 +211,9 @@ namespace Ref {
 
 		// Create subscribe socket
 		(void)snprintf(endpoint, 256, "tcp://%s:%d", this->m_hostname, this->m_serverPublishPort);
+		this->m_subSocket = zmq_socket(this->m_zmqContext, ZMQ_ROUTER);
 		zmq_setsockopt(this->m_subSocket, ZMQ_IDENTITY, (const char*)id, strlen((const char*)id));
-		NATIVE_INT_TYPE stat = zmq_connect(this->m_cmdSocket,endpoint);
+		stat = zmq_connect(this->m_subSocket,endpoint);
 		if (-1 == stat) {
 		  printf("ZMQBind Error\n");
 		  return;
@@ -181,6 +222,147 @@ namespace Ref {
     }
 
     void ZmqSocketIfImpl::socketReadTask(void* ptr){
+    	printf("socketReadTask\n");
+    	fflush(stderr);
+
+    	bool acceptConnections;
+        U32 packetDelimiter;
+        U32 packetSize;
+        U32 packetDesc;
+        U8 buf[FW_COM_BUFFER_MAX_SIZE];
+        U16 buf_ptr = 0;
+
+    	// cast pointer to component type
+    	ZmqSocketIfImpl* comp = (ZmqSocketIfImpl*) ptr;
+
+    	while(1){
+    		// Read incoming zmq message
+
+    		U32 msgSize = 0;
+			msgSize = zmqSocketRead(comp->m_subSocket, buf, (NATIVE_INT_TYPE)FW_COM_BUFFER_MAX_SIZE);
+
+			// Extract packet delimiter
+			packetDelimiter = *(U32*)(buf+buf_ptr);
+			packetDelimiter = ntohl(packetDelimiter);
+
+            // correct for network order
+            packetDelimiter = ntohl(packetDelimiter);
+            printf("Packet delimiter: 0x%04x\n",packetDelimiter);
+
+            // if magic number to quit, exit loop
+            if (packetDelimiter == 0xA5A5A5A5) {
+                (void) printf("packetDelimiter = 0x%x\n", packetDelimiter);
+                //break;
+            } else if (packetDelimiter != 0x5A5A5A5A) {
+                (void) printf("Unexpected delimiter 0x%08X\n",packetDelimiter);
+                // just keep reading until a delimiter is found
+                continue;
+            }
+
+            // Increment buffer pointer
+            buf_ptr += sizeof(packetDelimiter);
+
+
+            // Extract FPrime packet size
+            packetSize = *(U32*)(buf + buf_ptr);
+            packetSize = ntohl(packetSize);
+
+            printf("Packet Size: 0x%04x\n", packetSize);
+
+            // Increment buffer pointer
+            buf_ptr += sizeof(packetSize);
+
+
+            // Extract FPrime packet description
+            packetDesc = *(U32*)(buf + buf_ptr);
+            packetDesc = ntohl(packetDesc);
+
+            // Increment buffer pointer
+            buf_ptr += sizeof(packetDesc);
+
+            switch(packetDesc) {
+                case Fw::ComPacket::FW_PACKET_COMMAND:
+                	U8 cmdPacket[FW_COM_BUFFER_MAX_SIZE];
+
+                    // check size of command
+                    if (packetSize > FW_COM_BUFFER_MAX_SIZE) {
+                        (void) printf("Packet to large! :%d\n",packetSize);
+                        // might as well wait for the next packet
+                        break;
+                    }
+
+
+                    /*
+                    cmdBuffer[3] = packetDesc & 0xff;
+                    cmdBuffer[2] = (packetDesc & 0xff00) >> 8;
+                    cmdBuffer[1] = (packetDesc & 0xff0000) >> 16;
+                    cmdBuffer[0] = (packetDesc & 0xff000000) >> 24;
+					*/
+		    		
+		    		// Add description
+                    packetDesc = ntohl(packetDesc); // Is this the same as above?
+                    memcpy(cmdPacket, &packetDesc, sizeof(packetDesc));
+
+		    		// Add command data [ Size of cmd data is packet size - packetDesc size ]
+                    memcpy(cmdPacket + sizeof(packetDesc), buf + buf_ptr, packetSize - sizeof(packetDesc));
+
+
+                    if (comp->isConnected_uplinkPort_OutputPort(0)) {
+                         Fw::ComBuffer cmdBuffer(cmdPacket, packetSize);
+                         comp->uplinkPort_out(0,cmdBuffer,0);
+                    }
+                    break;
+            }
+/*
+                case Fw::ComPacket::FW_PACKET_FILE:
+                {
+
+                    // Get Buffer
+                    Fw::Buffer packet_buffer = comp->fileUplinkBufferGet_out(0, packetSize - sizeof(packetDesc));
+                    U8* data_ptr = (U8*)packet_buffer.getdata();
+
+
+                    // Read file packet minus description
+                    bytesRead = socketRead(comp->m_socketFd, data_ptr, packetSize - sizeof(packetDesc));
+                    if (-1 == bytesRead) {
+                        (void) printf("Size read error: %s\n",strerror(errno));
+                        break;
+                    }
+
+                    // for(uint32_t i =0; i < bytesRead; i++){
+                    //     (void) printf("IN_DATA:%02x\n", data_ptr[i]);
+                    // }
+
+                    if (comp->isConnected_fileUplinkBufferSendOut_OutputPort(0)) {
+                        comp->fileUplinkBufferSendOut_out(0, packet_buffer);
+                    }
+
+                }
+                    break;
+                default:
+                    FW_ASSERT(0);
+            }
+
+
+
+            } // while not done with packets
+            close(comp->m_socketFd);
+            comp->m_connectionFd = -1;
+
+            //try to re-establish connection with server
+            if (bytesRead == 0) {
+                comp->log_WARNING_LO_LostConnectionToServer(comp->port_number);
+                while (comp->m_connectionFd == -1){
+                    Os::Task::delay(5000);
+                	comp->openSocket(comp->port_number);
+                }
+            }
+            else {
+            	break;
+            }
+            */
+
+    	}
 
     }
 
