ab240b601e1cea40333b7f955ed5f6ef5703ebcd
ssinha <sanchit.sinha@jpl.nasa.gov>
2017-07-27 10:59:44 -0700

    Added setup script

--- Client-OMCT/server/src/static-server.js
+++ Client-OMCT/server/src/static-server.js
@@ -2,7 +2,7 @@ var express = require('express');
 function StaticServer(port) {
     var server = express();
     
-    server.use('/', express.static(__dirname + '/../..'));
+    server.use('/', express.static(__dirname + '/../../index-dev.html'));
     
     console.log('Open MCT hosted at http://localhost:' + port);
     
