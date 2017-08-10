5f0a02127c7e1f2e8c33a5ba5ee914652fe4ebc0
ssinha <sanchit.sinha@jpl.nasa.gov>
2017-08-10 16:04:52 -0700

    Implementing console into omct

---
+++ Client-OMCT/client/js/plugins/command-console.js
@@ -0,0 +1,44 @@
+function CommandConsole() {
+    return function install(openmct) {
+
+        openmct.objects.addRoot({
+            key: 'console',
+            namespace: 'isf.command'
+        });
+
+        openmct.objects.addProvider('isf.command', {
+            get: function () {
+                return Promise.resolve({
+                    name: 'Command Console',
+                    identifier: {
+                        key: 'console',
+                        namespace: 'isf.command'
+                    },
+                    type: 'isf.command'
+                });
+            }
+        });
+
+        openmct.mainViews.addProvider({
+            name: 'Command Console',
+            canView: function (d) {
+                return d.type === 'isf.command';
+            },
+            view: function (domainObject) {
+                var commandView = new CommandView();
+
+                return {
+                    show: function (container) {
+                        container.className += " abs";	// Open MCT bug requires this line
+                        
+                        commandView.$mount(container);
+                    },
+                    destroy: function (container) {
+                        commandView.$destroy();
+                    }
+                }
+            }
+        });
+    }
+}
+
--- Client-OMCT/client/js/vue-console.js
+++ Client-OMCT/client/js/vue-console.js
@@ -1,119 +1,80 @@
-function SetupConsole() {
-  const site = 'localhost';
-  const cmdPort = 1339;
+// const site = 'localhost';
+// const cmdPort = 1339;
 
-  var socket = new WebSocket('ws://' + site + ':' + cmdPort.toString());
-
-  function getCommands() {
-    // Returns promise of array of all commands
-    return getDictionary().then(function (dict) {
-      let commandDict = dict['isf']['commands'];       
-      let validCommands = [];
-      for (id in commandDict) {
-        let args = commandDict[id]['arguments'];
-        let argFormat = args.map((a) => a['name'] + ' (' + a['type'] + ') ');
-        validCommands.push({
-          'name': commandDict[id]['name'],
-          'args': argFormat
-        });
-      }
-      return validCommands;
-    });
-  }
-
-  Vue.directive('focus', {
-    bind: function () {
-      Vue.nextTick(function() {
-        object.focus();
-      });
-    }
-  });
-
-  var command = new Vue({
-    el: '#command',
-    data: {
+// var socket = new WebSocket('ws://' + site + ':' + cmdPort.toString());
+new Vue({
+// var CommandView = Vue.extend({
+  // template: $('#commandTemplate').text(),
+  el:'#console',
+  data: function () {
+    return {
       searchActive: false,
       cmd: '',
       cmdSearch: [],
       cmdHist: [],
       cmdHistQuery: '',
       cmdHistToShow: []
-      
-    },
-    methods: {
-      sendCmd: function (event) {
-        // Add command and time
-        socket.send(this.cmd);
-        this.cmdHist.push({
-          cmd: this.cmd,
-          time: Date()
-        });
-        this.cmdHistToShow = this.cmdHist.slice();
-        this.cmdSearch = [];  // Clear search
-      },
-      searchCmd: function (event) {
-        if (this.cmd !== '') {
-
-          self = this;  // Avoid 'this' scoping issues
-          getCommands().then(function (vc) {
-            self.cmdSearch = vc.filter((c) => c['name'].toLowerCase().indexOf(self.cmd.toLowerCase()) !== -1);
-          });
-
-        } else {
-          this.cmdSearch = [];
-        }
-
-        // Select from search results with arrow keys
-        // let i = 0;
-        // if (i >= 0 && i < this.cmdSearch.length) {
-        // // if(true) {
-        //     case 'ArrowDown': {
-        //       this.select(this.cmdSearch[i], false);
-        //       i += 1;
-
-        //       break;
-        //     } 
-        //     case 'ArrowUp': {
-        //       this.select(this.cmdSearch[i], false);
-        //       i -= 1;
-
-        //       break;
-        //     }
-        //     case 'Escape': {
-        //       this.cmdSearch = [];
-
-        //       break;
-        //     }
-
-        //     default: {
-        //       // Nothing todo
-        //       break;
-        //     }
-        //   }
-        // }
-      },
-      select: function (command, hist) {
-        if (hist) {
-          // If selection is from history, select entire command with arguments
-          this.cmd = command;
-        } else {
-          // Otherwise, just select command
-          this.cmd = command['name'] + '(';
-        }
-        
-      },
-      searchHist: function () {
-        if (this.cmdHistQuery !== '') {
-          this.cmdHistToShow = this.cmdHist.filter((c) => c.cmd.toLowerCase().indexOf(this.cmdHistQuery.toLowerCase()) !== -1);
-        } else {
-          this.cmdHistToShow = this.cmdHist.slice();  // Copy command history into buffer
-        }
-      }
-    },
-    computed: {
-      reverseCmd: function () {
-        return this.cmdHistToShow.reverse()
-      }
     }
-  });
-}
+  },
+  methods: {
+    getCommands: function () {
+      // Returns promise of array of all commands
+      return getDictionary().then(function (dict) {
+        let commandDict = dict['isf']['commands'];       
+        let validCommands = [];
+        for (id in commandDict) {
+          let args = commandDict[id]['arguments'];
+          let argFormat = args.map((a) => a['name'] + ' (' + a['type'] + ') ');
+          validCommands.push({
+            'name': commandDict[id]['name'],
+            'args': argFormat
+          });
+        }
+        return validCommands;
+      });
+    },
+    sendCmd: function (event) {
+      // Add command and time
+      socket.send(this.cmd);
+      this.cmdHist.push({
+        cmd: this.cmd,
+        time: Date()
+      });
+      this.cmdHistToShow = this.cmdHist.slice();
+      this.cmdSearch = [];  // Clear search
+    },
+    searchCmd: function (event) {
+      if (this.cmd !== '') {
+
+        self = this;  // Avoid 'this' scoping issues
+        self.getCommands().then(function (vc) {
+          self.cmdSearch = vc.filter((c) => c['name'].toLowerCase().indexOf(self.cmd.toLowerCase()) !== -1);
+        });
+
+      } else {
+        this.cmdSearch = [];
+      }
+    },
+    select: function (command, hist) {
+      if (hist) {
+        // If selection is from history, select entire command with arguments
+        this.cmd = command;
+      } else {
+        // Otherwise, just select command
+        this.cmd = command['name'] + '(';
+      }
+    },
+    searchHist: function () {
+      if (this.cmdHistQuery !== '') {
+        this.cmdHistToShow = this.cmdHist.filter((c) => c.cmd.toLowerCase().indexOf(this.cmdHistQuery.toLowerCase()) !== -1);
+      } else {
+        this.cmdHistToShow = this.cmdHist.slice();  // Copy command history into buffer
+      }
+    },
+  },
+  computed: {
+    reverseCmd: function () {
+      return this.cmdHistToShow.reverse()
+    }
+  }
+});
