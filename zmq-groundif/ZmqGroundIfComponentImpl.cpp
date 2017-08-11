19ae342656f43d0d15f0fc563f110c3a0540c28f
David <david.kooi@jpl.nasa.gov>
2017-08-11 15:21:46 -0700

    Added copyright header

--- zmq-groundif/ZmqGroundIfComponentImpl.cpp
+++ zmq-groundif/ZmqGroundIfComponentImpl.cpp
@@ -1,9 +1,28 @@
+// ====================================================================== 
+// \title  ZmqGroundIfComponentImpl.cpp
+// \author dkooi
+// \brief  cpp file for ZmqGroundIf implementation class.
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged. Any commercial use must be negotiated with the Office
+// of Technology Transfer at the California Institute of Technology.
+// 
+// This software may be subject to U.S. export control laws and
+// regulations.  By accepting this document, the user agrees to comply
+// with all U.S. export laws and regulations.  User has the
+// responsibility to obtain export licenses, or other export authority
+// as may be required before exporting such information to foreign
+// countries or providing access to foreign persons.
+// ====================================================================== 
+
+
 #include <Fw/Types/BasicTypes.hpp>
-#include <fprime-zmq/ZmqGroundIf/ZmqGroundIfComponentImpl.hpp>
+#include <fprime-zmq/zmq-groundif/ZmqGroundIfComponentImpl.hpp>
 
 #define DEBUG_PRINT(x,...) printf(x,##__VA_ARGS__)
 //#define DEBUG_PRINT(x,...)
-
 
 namespace Zmq{
 
--- zmq-groundif/ZmqGroundIfComponentImpl.hpp
+++ zmq-groundif/ZmqGroundIfComponentImpl.hpp
@@ -1,10 +1,27 @@
-
+// ====================================================================== 
+// \title  ZmqGroundIfComponentImpl.hpp
+// \author dkooi
+// \brief  hpp file for ZmqGroundIf implementation class.
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged. Any commercial use must be negotiated with the Office
+// of Technology Transfer at the California Institute of Technology.
+// 
+// This software may be subject to U.S. export control laws and
+// regulations.  By accepting this document, the user agrees to comply
+// with all U.S. export laws and regulations.  User has the
+// responsibility to obtain export licenses, or other export authority
+// as may be required before exporting such information to foreign
+// countries or providing access to foreign persons.
+// ====================================================================== 
 
 
 #ifndef ZMQGROUNDIFIMPL_HPP_
 #define ZMQGROUNDIFIMPL_HPP_
 
-#include <fprime-zmq/ZmqGroundIf/ZmqGroundIfComponentAc.hpp>
+#include <fprime-zmq/zmq-groundif/ZmqGroundIfComponentAc.hpp>
 
 namespace Zmq{
 
