a0cf3ab0ffbff469d1fa3984e283c2084258d801
Jordan Ishii <jordan.ishii@jpl.nasa.gov>
2018-05-29 16:04:47 -0700

    Added GUI screens for EVR and Channel views, Added Event ruby file for data_viewer application, added multiple telemetry and command fields, set length to auto for all telemetry definitions

--- COSMOS/config/targets/REF/lib/evr_dump_component.rb
+++ COSMOS/config/targets/REF/lib/evr_dump_component.rb
@@ -13,18 +13,28 @@ require 'cosmos/tools/data_viewer/data_viewer_component'
 
 module Cosmos
   # This class displays packets as raw hex values
-  class InstDumpComponent < DataViewerComponent
+  class EvrDumpComponent < DataViewerComponent
+      
+    # Prints the header strings for EVR's
+    def initialize_gui
+        super
+        @text.appendPlainText("TIME\t\t\tNAME\t\tID\tSEVERITY\tMESSAGE\n" << '-' * 100)
+    end
+      
+        
+      #@text.appendPlainText("Test")
 
     # Processes the given packet. No gui interaction should be done in this
     # method. Override this method for other components.
     def process_packet (packet)
       processed_text = ''
-      processed_text << '*' * 80 << "\n"
-      processed_text << "* #{packet.target_name} #{packet.packet_name}\n"
-      processed_text << "* Received Time: #{packet.received_time.formatted}\n" if packet.received_time
-      processed_text << "* Received Count: #{packet.received_count}\n"
-      processed_text << '*' * 80 << "\n"
-      processed_text << packet.buffer.formatted << "\n"
+      processed_text << "\n"
+      processed_text << "#{packet.received_time.formatted}\t"
+      processed_text << "#{packet.packet_name}\t"
+      processed_text << "#{tlm_variable(packet.target_name + ' ' + packet.packet_name + ' OP_CODE', :RAW)}\t"
+      processed_text << "COMMAND\t" # Hardcoded change later
+      processed_text << "#{tlm_variable(packet.target_name + ' ' + packet.packet_name + ' MESSAGE', :RAW)}\t"
+
       # Ensure that queue does not grow infinitely while paused
       if @processed_queue.length < 1000
         @processed_queue << processed_text
