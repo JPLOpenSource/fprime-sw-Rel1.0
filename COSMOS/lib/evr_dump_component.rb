7a2118f3bcd4a6792dd10e993bed7a37b51885e1
Jordan Ishii <jordan.ishii@jpl.nasa.gov>
2018-05-31 16:02:55 -0700

    Various styling fixes for Ref channel screen and added new telemetry using configuration edge-cases to COSMOS

--- COSMOS/lib/evr_dump_component.rb
+++ COSMOS/lib/evr_dump_component.rb
@@ -13,7 +13,8 @@ module Cosmos
     # Prints the header strings for EVR's
     def initialize_gui
         super
-        @text.appendPlainText("TIME\t\t\tNAME\t\tID\tSEVERITY\tMESSAGE\n" << '-' * 100)
+        @text.font = Cosmos.get_default_font
+        @text.appendPlainText("TIME\t\t\tNAME\t\t\tID\tSEVERITY\t\tMESSAGE\n" << '-' * 115)
     end
       
         
@@ -22,12 +23,22 @@ module Cosmos
     # Processes the given packet. No gui interaction should be done in this
     # method. Override this method for other components.
     def process_packet (packet)
+      # Determine tab amount between columns
+      sev_tabs = 2
+      name_tabs = 2
+      if "#{packet.packet_name}".length > (8 * 2)
+          name_tabs = name_tabs - 1
+      end
+      if "#{tlm_variable(packet.target_name + ' ' + packet.packet_name + ' SEVERITY', :RAW)}".length > (8 * 1)
+          sev_tabs = sev_tabs - 1
+      end
+      
       processed_text = ''
       processed_text << "\n"
       processed_text << "#{packet.received_time.formatted}\t"
-      processed_text << "#{packet.packet_name}\t"
+      processed_text << "#{packet.packet_name}" << "\t" * name_tabs
       processed_text << "#{tlm_variable(packet.target_name + ' ' + packet.packet_name + ' EVR_ID', :RAW)}\t"
-      processed_text << "COMMAND\t" # Hardcoded change later
+      processed_text << "#{tlm_variable(packet.target_name + ' ' + packet.packet_name + ' SEVERITY', :RAW)}" << "\t" * sev_tabs
       processed_text << "#{tlm_variable(packet.target_name + ' ' + packet.packet_name + ' MESSAGE', :RAW)}\t"
 
       # Ensure that queue does not grow infinitely while paused
